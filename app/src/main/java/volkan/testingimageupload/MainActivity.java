0d1d4d08f4dece7a53f05131ee822c53af1dd948
Volcano28 <volkanoztuzun@gmail.com>
2015-05-02 08:00:07 -0400

    Dithering Update
    Dithering algorithm is up and running.

--- app/src/main/java/volkan/testingimageupload/MainActivity.java
+++ app/src/main/java/volkan/testingimageupload/MainActivity.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Random;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -39,7 +40,13 @@ import android.content.Context;
 public class MainActivity extends Activity {
     private Uri mImageCaptureUri;
     private ImageView mImageView;
+    private Bitmap bitmap;
     private Bitmap operation;
+    public double[] threshold = { 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31,
+            0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4, 0.41, 0.42,
+            0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5, 0.51, 0.52, 0.53,
+            0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.6, 0.61, 0.62, 0.63, 0.64,
+            0.65, 0.66, 0.67, 0.68, 0.69 };
 
     private static final int PICK_FROM_CAMERA = 1;
     private static final int PICK_FROM_FILE = 2;
@@ -100,115 +107,127 @@ public class MainActivity extends Activity {
     @Override
     protected void onActivityResult(int reqCode, int resCode, Intent data) {
 
-            if(reqCode == PICK_FROM_FILE) {
-                try {
-                    mImageView = (ImageView) findViewById(R.id.iv_pic);
-                    Uri picuri = data.getData();
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), picuri);
+        if(reqCode == PICK_FROM_FILE) {
+            try {
+                mImageView = (ImageView) findViewById(R.id.iv_pic);
+                Uri picuri = data.getData();
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), picuri);
 
-                    operation= Bitmap.createBitmap(bitmap.getWidth(),
-                            bitmap.getHeight(), bitmap.getConfig());
-                   /* int fac = 2;
-                    for(int i=0; i<bitmap.getWidth(); i++){
-                        for(int j=0; j<bitmap.getHeight(); j++){
-                            int p = bitmap.getPixel(i, j);
-                            int r = Color.red(p);
-                            int g = Color.green(p);
-                            int b = Color.blue(p);
-                            int alpha = Color.alpha(p);
-                            if (r * fac > 255){
-                            r = 255;
-                            } else {
-                                r = fac * r;
-                            }
-                            if (g * fac > 255){
-                                g = 255;
-                            } else {
-                                g = fac * g;
-                            }
-                            if (b * fac > 255){
-                                b = 255;
-                            } else {
-                                b = fac * b;
-                            }
-                            if (alpha * fac > 255){
-                                alpha = 255;
-                            } else {
-                                alpha = fac * alpha;
-                            }
+                operation= Bitmap.createBitmap(bitmap.getWidth(),
+
+                        bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+                mImageView.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException f) {
+                // Bitmap bmp=BitmapFactory.decodeStream(getContentResolver().openInputStream(data.getData()));
+                //  mImageView.setImageURI(data.getData());
+                //  mImageView.setImageBitmap(bmp);
+            } catch (IOException O) {
 
 
-                            r = 100  +  r;
-                            g = 100  + g;
-                            b = 100  + b;
-                            alpha = 100 + alpha;
+            }
+        }
+    }
 
-                            operation.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
-                        } */
-                    int rd;
-                    int gr;
-                    int bl;
-                    double red = 0.30;
-                    double green = 0.59;
-                    double blue = 0.11;
+    public void Gray(View view){
 
-                    for(int i=0; i<bitmap.getWidth(); i++){
-                        for(int j=0; j<bitmap.getHeight(); j++){
-                            int p = bitmap.getPixel(i, j);
-                            int r = Color.red(p);
-                            int g = Color.green(p);
-                            int b = Color.blue(p);
+        operation= Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), bitmap.getConfig());
+        int rd;
+        int gr;
+        int bl;
+        double red = 0.30;
+        double green = 0.59;
+        double blue = 0.11;
 
-                            /*rd = (int) red * r;
-                            gr = (int) green * g;
-                            bl = (int) blue * b;*/
+        for(int i=0; i<bitmap.getWidth(); i++){
+            for(int j=0; j<bitmap.getHeight(); j++){
+                int p = bitmap.getPixel(i, j);
+                int r = Color.red(p);
+                int g = Color.green(p);
+                int b = Color.blue(p);
 
-                            r = g = b = (int)(red * r + g * green + b * blue);
 
-                            operation.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
-                        }
-                    }
+                r = g = b = (int)(red * r + g * green + b * blue);
 
-                    mImageView.setImageBitmap(operation);
+                operation.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
+            }
+        }
+        mImageView.setImageBitmap(operation);
+    }
 
-                } catch (FileNotFoundException f) {
-                    // Bitmap bmp=BitmapFactory.decodeStream(getContentResolver().openInputStream(data.getData()));
-                    //  mImageView.setImageURI(data.getData());
-                    //  mImageView.setImageBitmap(bmp);
-                } catch (IOException O) {
+    public void Brightness(View view){
 
+        operation= Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), bitmap.getConfig());
+        int fac = 2;
+        int value = 50;
+        for(int i=0; i<bitmap.getWidth(); i++) {
+            for (int j = 0; j < bitmap.getHeight(); j++) {
+                int p = bitmap.getPixel(i, j);
+                int r = Color.red(p);
+                int g = Color.green(p);
+                int b = Color.blue(p);
+                int alpha = Color.alpha(p);
+
+                r += value;
+                if( r > 255 ){
+                    r = 255;
+                } else if ( r < 0 ){
+                    r = 0;
+                }
+
+                g += value;
+                if( g > 255 ){
+                    g = 255;
+                } else if ( g < 0 ){
+                    g = 0;
+                }
+
+                b += value;
+                if( b > 255 ){
+                    b = 255;
+                } else if ( b < 0 ){
+                    b = 0;
+                }
+
+                operation.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
+            }
+        }
+        mImageView.setImageBitmap(operation);
+    }
+
+    public void Dithering(View view) {
+        operation = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Random randomnumber = new Random();
+
+        for (int i = 0; i < bitmap.getWidth(); i++) {
+            for (int j = 0; j < bitmap.getHeight(); j++) {
+
+                int color = bitmap.getPixel(i, j);
+                int red = (color >>> 16) & 0xFF;
+                int green = (color >>> 8) & 0xFF;
+                int blue = color & 0xFF;
+
+                double lim = (red * 0.21f + green * 0.71f + blue * 0.07f) / 255;
+
+
+                if (lim <= threshold[randomnumber.nextInt(threshold.length)]) {
+
+                    operation.setPixel(i, j, Color.argb(Color.alpha(color), 0x000000, 0x000000, 0x000000));
+
+                } else {
+
+                    operation.setPixel(i, j, Color.argb(Color.alpha(color), 0xFFFFFF, 0xFFFFFF, 0xFFFFFF));
 
                 }
             }
 
-        /*if (resultCode != RESULT_OK) return;
-
-        Bitmap bitmap 	= null;
-        String path		= "";
-        mImageView = (ImageView) findViewById(R.id.iv_pic);
-        mImageView.setImageURI(data.getData());
-        if (requestCode == PICK_FROM_FILE) {
-            mImageCaptureUri = data.getData();
-            //path = getRealPathFromURI(mImageCaptureUri); //from Gallery
-            //Toast.makeText(this, path,Toast.LENGTH_LONG).show();
-            //if (path == null)
-                path = mImageCaptureUri.getPath(); //from File Manager
-
-            if (path != null){
-              //  bitmap 	= BitmapFactory.decodeFile(path);
-
-                Toast.makeText(this, path,Toast.LENGTH_LONG).show();
-            }
-
-
-            mImageView.setImageBitmap(bitmap);
-            Toast.makeText(this, data.getData().toString(),Toast.LENGTH_LONG).show();
-        } else {
-            path	= mImageCaptureUri.getPath();
-            bitmap  = BitmapFactory.decodeFile(path);
         }
+        mImageView.setImageBitmap(operation);
 
-        mImageView.setImageBitmap(bitmap);*/
     }
 
     public String getRealPathFromURI(Uri contentUri) {
